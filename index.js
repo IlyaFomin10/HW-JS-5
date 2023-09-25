f4c70f8891e0a4c8fbfffbc49572c40780630aad
Ilya <fominiv10@gmail.com>
2023-09-26 00:24:04 +0300

    HW5 decided  3 task  commit

--- index.js
+++ index.js
@@ -128,3 +128,54 @@ for (const key in numbers) {
 }
 
 // Task2
+const post = {
+  author: "John", // вывести этот текст
+  postId: 23,
+  comments: [
+    {
+      userId: 10,
+      userName: "Alex",
+      text: "lorem ipsum",
+      rating: {
+        likes: 10,
+        dislikes: 2, // вывести это число
+      },
+    },
+    {
+      userId: 5, // вывести это число
+      userName: "Jane",
+      text: "lorem ipsum 2", // вывести этот текст
+      rating: {
+        likes: 3,
+        dislikes: 1,
+      },
+    },
+  ],
+};
+console.log(post.author);
+console.log(post.comments[0]["rating"]["dislikes"]);
+console.log(post.comments[1]["userId"]);
+console.log(post.comments[1]["text"]);
+
+// Task3
+const products = [
+  {
+    id: 3,
+    price: 200,
+  },
+  {
+    id: 4,
+    price: 900,
+  },
+  {
+    id: 1,
+    price: 1000,
+  },
+];
+
+products.forEach((element) => {
+  element["price"] *= 0.85;
+  console.log(element["price"]);
+});
+
+// Task4
